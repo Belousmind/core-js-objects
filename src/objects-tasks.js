663ab0f47342a7675e42b3698c4d7394216a6121
belousmind <belousmind@gmail.com>
2025-02-26 16:00:27 +0300

    feat: solve sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -174,8 +174,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -219,8 +219,15 @@ function fromJSON(/* proto, json */) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+
+function sortCitiesArray(arr) {
+  const sortedArr = arr.sort((a, b) => {
+    const countryComparison = a.country.localeCompare(b.country);
+    return countryComparison !== 0
+      ? countryComparison
+      : a.city.localeCompare(b.city);
+  });
+  return sortedArr;
 }
 
 /**
